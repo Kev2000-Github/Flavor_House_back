bfd412d84d565af06a45f267002f78244b654d4e
chengkev <chengkev2000@gmail.com>
2023-07-16 11:38:52 -0400

    fix: add comment on config regex formatting

--- src/config/index.js
+++ src/config/index.js
@@ -3,8 +3,11 @@ const ajv = new Ajv({allErrors: true})
 const addFormats = require('ajv-formats')
 const addErrors = require('ajv-errors')
 addFormats(ajv)
+//8 characters min, 1+ lowercase, 1+ uppercase, 1+ special character, alphanumerical string
 ajv.addFormat('password', /^[a-zA-Z0-9_@#$%^*()-]{8,}$/)
+//no number nor special characters
 ajv.addFormat('fullName', /^[a-zA-Z]+( [a-zA-Z]+)*$/)
+//no spaces in username
 ajv.addFormat('username', /^[a-zA-Z0-9_@#$%^*()-]+$/)
 addErrors(ajv)
 
