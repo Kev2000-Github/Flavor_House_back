47001a82a8839380cdb9759e81d651327049b50b
chengkev <chengkev2000@gmail.com>
2023-08-10 18:53:21 -0400

    fix: refactor authentication middleware

--- src/middlewares/authentication.middleware.js
+++ src/middlewares/authentication.middleware.js
@@ -2,12 +2,15 @@ const config = require('../config')
 const { HttpStatusError } = require('../errors/httpStatusError')
 const { controllerWrapper } = require('../utils/common')
 const jwt = require('jsonwebtoken')
+const {Users} = require('../database/models')
 
-const authentication = controllerWrapper((req, res, next) => {
+const authentication = controllerWrapper(async (req, res, next) => {
     const bearerToken = req.headers['authorization']
     if(!bearerToken) throw HttpStatusError.unauthorize({message: 'A token is required for authentication'})
     const token = bearerToken.split(' ').pop()
     const decoded = jwt.verify(token, config.JWT_TOKEN)
+    const user = await Users.findByPk(decoded.id)
+    if(!user) throw HttpStatusError.unauthorize({message: 'invalid session'})
     req.user = decoded
     next()
 })
