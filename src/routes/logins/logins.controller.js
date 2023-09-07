0e7f1b5c86dabc5be1791d81bc4bd3bf8e72fe53
GDJTS23 <gdjtorrealba23@gmail.com>
2023-09-06 22:47:11 -0400

    detalles resueltos

--- src/routes/logins/logins.controller.js
+++ src/routes/logins/logins.controller.js
@@ -1,10 +1,9 @@
 const {Users} = require('../../database/models')
 const { messages } = require('./messages')
-const bcrypt = require ('bcrypt')
 const config = require('../../config')
 const jwt = require('jsonwebtoken')
 const { HttpStatusError } = require('../../errors/httpStatusError')
-const { controllerWrapper } = require('../../utils/common')
+const { controllerWrapper, verifyPassword } = require('../../utils/common')
 const { responseData } = require('../users/helper')
 
 module.exports.post_logins = controllerWrapper(async (req, res) => {
@@ -13,9 +12,8 @@ module.exports.post_logins = controllerWrapper(async (req, res) => {
 
     const user = await Users.findOne({where:{email}})
     if(!user) throw HttpStatusError.notFound(messages.notFound)
-
-    const passwordval = bcrypt.compareSync (password ,user.password)
-    if (!passwordval) throw HttpStatusError.notFound(messages.notFound)
+    
+    if (!(await verifyPassword(password,user.password))) throw HttpStatusError.notFound(messages.notFound)
     const data = responseData(user)
     const token = jwt.sign(
         data,
--- src/routes/logins/messages.js
+++ src/routes/logins/messages.js
@@ -3,10 +3,6 @@ module.exports = {
         notFound: {
             code: 'login_NF',
             message: 'incorrect credentials'
-        },
-        forbidden: {
-            code: 'login_FB',
-            message: 'You do not have permissions to perform this action'
         }
     }
 }
