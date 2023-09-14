070d45db42345776a8c2d85f06d8c5d01c9dcb44
Shely Zencoff <cristzencoff@gmail.com>
2023-09-14 15:36:07 -0300

    fix: responses and pagination

--- src/routes/posts/posts.controller.js
+++ src/routes/posts/posts.controller.js
@@ -1,44 +1,53 @@
-const { 
-    Posts, 
-    ViewPostsLikes, 
-    Recipes, 
+const {
+    Posts,
+    ViewPostsLikes,
+    Recipes,
     Moments,
-    Users, 
-    Favorites, 
-    Likes, 
-    Interests, 
+    Users,
+    Favorites,
+    Likes,
+    Interests,
     Ingredients,
     Steps,
     ViewRecipeStars,
-    sequelize} = require('../../database/models')
+    sequelize,
+} = require('../../database/models')
 const { controllerWrapper } = require('../../utils/common')
-const {paginate} = require('../../database/helper')
-const {isEditable, getRecipeSearchOpts, formatOrder, recipeResponseData} = require('./helper')
+const { paginate } = require('../../database/helper')
+const {
+    isEditable,
+    getRecipeSearchOpts,
+    formatOrder,
+    recipeResponseData,
+} = require('./helper')
 const { HttpStatusError } = require('../../errors/httpStatusError')
-const {messages} = require('./messages')
+const { messages } = require('./messages')
 const { POST_TYPE } = require('../../database/constants')
 const uuid = require('uuid').v4
 
 const includeOpts = (userId, tag = true) => {
-    const val =  {
-        include: [{
-            model: Posts,
-            include: [
-                Users,
-                ViewPostsLikes,
-                {
-                    model: Favorites,
-                    required: false,
-                    where: { userId }
-                },
-                {
-                    model: Likes,
-                    required: false,
-                    where: { userId }
-                }
-            ]
-        }]}
-    if(tag) val.include.push({model: Interests, as: 'Tags'})
+    const val = {
+        include: [
+            {
+                model: Posts,
+                include: [
+                    Users,
+                    ViewPostsLikes,
+                    {
+                        model: Favorites,
+                        required: false,
+                        where: { userId },
+                    },
+                    {
+                        model: Likes,
+                        required: false,
+                        where: { userId },
+                    },
+                ],
+            },
+        ],
+    }
+    if (tag) val.include.push({ model: Interests, as: 'Tags' })
     return val
 }
 
@@ -47,122 +56,130 @@ module.exports.get_posts_recipe = controllerWrapper(async (req, res) => {
     const pagination = req.pagination
     const order = formatOrder(req.query?.order)
     let opts = await getRecipeSearchOpts(req.query?.search, req.query?.tags)
-    if(opts.include.length === 0){
-        opts = {...opts, ...includeOpts(userId)}
+    if (opts.include.length === 0) {
+        opts = { ...opts, ...includeOpts(userId) }
+    } else {
+        opts.include.push(includeOpts(userId, false).include[0])
     }
-    else{
-        opts.include.push((includeOpts(userId, false)).include[0])
-    }
-    if(order) opts.order = [['createdAt', order]]
-    opts = {...opts, ...pagination}
+    if (order) opts.order = [['createdAt', order]]
+    opts = { ...opts, ...pagination }
     opts.include.push(ViewRecipeStars)
     const posts = await paginate(Recipes, opts)
-    posts.data = posts.data.map(post => recipeResponseData(post))
+    posts.data = posts.data.map((post) => recipeResponseData(post))
     res.json(posts)
 })
 
 module.exports.get_posts_recipe_id = controllerWrapper(async (req, res) => {
-    const {id} = req.params
+    const { id } = req.params
     const userId = req.user.id
     const post = await Recipes.findByPk(id, includeOpts(userId))
-    if(!post) throw HttpStatusError.notFound(messages.notFound)
+    if (!post) throw HttpStatusError.notFound(messages.notFound)
     res.json(recipeResponseData(post))
 })
 
 module.exports.post_posts_recipe = controllerWrapper(async (req, res) => {
-    const {title, description, tags, ingredients, steps, picture} = req.body
+    const { title, description, tags, ingredients, steps, picture } = req.body
     const userId = req.user.id
     const postId = uuid()
-    await sequelize.transaction(async transaction => {
-        const recipeIngredients = ingredients.map(ingredient => ({
+    await sequelize.transaction(async (transaction) => {
+        const recipeIngredients = ingredients.map((ingredient) => ({
             id: uuid(),
             recipeId: postId,
-            description: ingredient
+            description: ingredient,
         }))
-        const recipeSteps = steps.map(step => ({
+        const recipeSteps = steps.map((step) => ({
             id: uuid(),
             recipeId: postId,
             description: step.description,
-            image: step.picture
+            image: step.picture,
         }))
-        await Posts.create({
-            id: postId,
-            madeBy: userId,
-            type: POST_TYPE.RECIPE
-        }, {transaction})
-        const recipes = await Recipes.create({
-            postId,
-            name: title,
-            description,
-            image: picture
-        }, {transaction})
-        await recipes.addTags(tags, {transaction})
-        await Ingredients.bulkCreate(recipeIngredients, {transaction})
-        await Steps.bulkCreate(recipeSteps, {transaction})
+        await Posts.create(
+            {
+                id: postId,
+                madeBy: userId,
+                type: POST_TYPE.RECIPE,
+            },
+            { transaction }
+        )
+        const recipes = await Recipes.create(
+            {
+                postId,
+                name: title,
+                description,
+                image: picture,
+            },
+            { transaction }
+        )
+        await recipes.addTags(tags, { transaction })
+        await Ingredients.bulkCreate(recipeIngredients, { transaction })
+        await Steps.bulkCreate(recipeSteps, { transaction })
     })
     const post = await Recipes.findByPk(postId, includeOpts(userId))
-    if(!post) throw HttpStatusError.notFound(messages.notFound)
+    if (!post) throw HttpStatusError.notFound(messages.notFound)
     res.json(recipeResponseData(post))
 })
 
 module.exports.put_posts_recipe_id = controllerWrapper(async (req, res) => {
-    const {title, description, tags, ingredients, steps, picture} = req.body
+    const { title, description, tags, ingredients, steps, picture } = req.body
     const userId = req.user.id
     const postId = req.params.id
     const recipe = await Recipes.findByPk(postId, { include: [Posts] })
-    if(!recipe) throw HttpStatusError.notFound(messages.notFound)
-    if(recipe.Post.madeBy !== userId) throw HttpStatusError.forbidden(messages.notOwner)
-    if(!isEditable(recipe.createdAt)){
+    if (!recipe) throw HttpStatusError.notFound(messages.notFound)
+    if (recipe.Post.madeBy !== userId)
+        throw HttpStatusError.forbidden(messages.notOwner)
+    if (!isEditable(recipe.createdAt)) {
         throw HttpStatusError.notFound(messages.notEditable)
     }
-    await sequelize.transaction(async transaction => {
-        //INGREDIENTS
-        const recipeIngredients = ingredients.map(ingredient => ({
+    await sequelize.transaction(async (transaction) => {
+    //INGREDIENTS
+        const recipeIngredients = ingredients.map((ingredient) => ({
             id: uuid(),
             recipeId: postId,
-            description: ingredient
+            description: ingredient,
         }))
         //STEPS
-        const recipeSteps = steps.map(step => ({
+        const recipeSteps = steps.map((step) => ({
             id: uuid(),
             recipeId: postId,
             description: step.description,
-            image: step.picture
+            image: step.picture,
         }))
         //GENERAL INFORMATION
-        await recipe.update({
-            name: title,
-            description,
-            image: picture
-        }, {transaction})
+        await recipe.update(
+            {
+                name: title,
+                description,
+                image: picture,
+            },
+            { transaction }
+        )
         //TAGS
-        await recipe.setTags(tags, {transaction})
-        await Ingredients.destroy({where: {recipeId: postId}, transaction})
-        await Ingredients.bulkCreate(recipeIngredients, {transaction})
-        await Steps.destroy({where: {recipeId: postId}, transaction})
-        await Steps.bulkCreate(recipeSteps, {transaction})
+        await recipe.setTags(tags, { transaction })
+        await Ingredients.destroy({ where: { recipeId: postId }, transaction })
+        await Ingredients.bulkCreate(recipeIngredients, { transaction })
+        await Steps.destroy({ where: { recipeId: postId }, transaction })
+        await Steps.bulkCreate(recipeSteps, { transaction })
     })
     const updatedPost = await Recipes.findByPk(postId, includeOpts(userId))
-    if(!updatedPost) throw HttpStatusError.notFound(messages.notFound)
+    if (!updatedPost) throw HttpStatusError.notFound(messages.notFound)
     res.json(recipeResponseData(updatedPost))
 })
-
 
 module.exports.delete_posts_recipe_id = controllerWrapper(async (req, res) => {
     const postId = req.params.id
     const userId = req.user.id
     const recipe = await Recipes.findByPk(postId, { include: [Posts] })
-    if(!recipe) throw HttpStatusError.notFound(messages.notFound)
-    if(recipe.Post.madeBy !== userId) throw HttpStatusError.forbidden(messages.notOwner)
-    await sequelize.transaction(async transaction => {
-        await recipe.setTags([], {transaction})
-        await Ingredients.destroy({where: {recipeId: postId}, transaction})
-        await Steps.destroy({where: {recipeId: postId}, transaction})
-        await recipe.destroy({transaction})
+    if (!recipe) throw HttpStatusError.notFound(messages.notFound)
+    if (recipe.Post.madeBy !== userId)
+        throw HttpStatusError.forbidden(messages.notOwner)
+    await sequelize.transaction(async (transaction) => {
+        await recipe.setTags([], { transaction })
+        await Ingredients.destroy({ where: { recipeId: postId }, transaction })
+        await Steps.destroy({ where: { recipeId: postId }, transaction })
+        await recipe.destroy({ transaction })
     })
-    res.json({deleted: true})
+    res.json({ deleted: true })
 })
-
 
 //moments
 
@@ -171,67 +188,83 @@ module.exports.get_posts_moment = controllerWrapper(async (req, res) => {
 
     const pagination = req.pagination
     const order = formatOrder(req.query?.order)
-    let opts = { ...includeOpts(userId,false), pagination}
-    if(order) opts.order = [['createdAt', order]]
+    let opts = { ...includeOpts(userId, false), ...pagination }
+    if (order) opts.order = [['createdAt', order]]
     const moments = await paginate(Moments, opts)
+    moments.data = moments.data.map(post => recipeResponseData(post)) 
     res.json(moments)
+ 
 })
 
 module.exports.get_posts_moment_id = controllerWrapper(async (req, res) => {
-  
     const userId = req.user.id
     const postId = req.params.id
-    const moment = await Moments.findByPk(postId, includeOpts(userId,false))
-    if(!moment) throw HttpStatusError.notFound(messages.notFound)
-    res.json(moment)  // Or use the momentResponseData function if you have one
+    const moment = await Moments.findByPk(postId, includeOpts(userId, false))
+    if (!moment) throw HttpStatusError.notFound(messages.notFound)
+    moment.data = recipeResponseData(moment)
+    res.json(moment)
 })
 
 module.exports.post_posts_moment = controllerWrapper(async (req, res) => {
-    const {description, image} = req.body
+    const { description, image } = req.body
     const userId = req.user.id
     const postId = uuid()
 
-    await sequelize.transaction(async transaction => {
-        await Posts.create({
-            id: postId,
-            madeBy: userId,
-            type: POST_TYPE.MOMENT
-        }, {transaction})
+    await sequelize.transaction(async (transaction) => {
+        await Posts.create(
+            {
+                id: postId,
+                madeBy: userId,
+                type: POST_TYPE.MOMENT,
+            },
+            { transaction }
+        )
 
-        await Moments.create({
-            postId,
-            description,
-            image
-        }, {transaction})
+        await Moments.create(
+            {
+                postId,
+                description,
+                image,
+            },
+            { transaction }
+        )
     })
 
-    const moment = await Moments.findByPk(postId, includeOpts(userId,false))
-    if(!moment) throw HttpStatusError.notFound(messages.notFound)
-    res.json(moment)  // Or use the momentResponseData function
+    const moment = await Moments.findByPk(postId, includeOpts(userId, false))
+    if (!moment) throw HttpStatusError.notFound(messages.notFound)
+    moment.data = recipeResponseData(moment)
+    res.json(moment)
 })
 
 module.exports.put_posts_moment_id = controllerWrapper(async (req, res) => {
-    const {description, image} = req.body
+    const { description, image } = req.body
     const userId = req.user.id
     const postId = req.params.id
     const moment = await Moments.findByPk(postId, { include: [Posts] })
 
-    if(!moment) throw HttpStatusError.notFound(messages.notFound)
-    if(moment.Post.madeBy !== userId) throw HttpStatusError.forbidden(messages.notOwner)
-    if(!isEditable(moment.createdAt)){
+    if (!moment) throw HttpStatusError.notFound(messages.notFound)
+    if (moment.Post.madeBy !== userId)
+        throw HttpStatusError.forbidden(messages.notOwner)
+    if (!isEditable(moment.createdAt)) {
         throw HttpStatusError.notFound(messages.notEditable)
     }
 
-    await sequelize.transaction(async transaction => {
-        await moment.update({
+    await moment.update(
+        {
             description,
-            image
-        }, {transaction})
-    })
+            image,
+        }
+        
+    )
 
-    const updatedMoment = await Moments.findByPk(postId, includeOpts(userId,false))
-    if(!updatedMoment) throw HttpStatusError.notFound(messages.notFound)
-    res.json(updatedMoment)  // Or use the momentResponseData function
+
+    const updatedMoment = await Moments.findByPk(
+        postId,
+        includeOpts(userId, false)
+    )
+    if (!updatedMoment) throw HttpStatusError.notFound(messages.notFound)
+    updatedMoment.data = recipeResponseData(updatedMoment)
+    res.json(updatedMoment) // Or use the momentResponseData function
 })
 
 module.exports.delete_posts_moment_id = controllerWrapper(async (req, res) => {
@@ -239,12 +272,14 @@ module.exports.delete_posts_moment_id = controllerWrapper(async (req, res) => {
     const userId = req.user.id
     const moment = await Moments.findByPk(postId, { include: [Posts] })
 
-    if(!moment) throw HttpStatusError.notFound(messages.notFound)
-    if(moment.Post.madeBy !== userId) throw HttpStatusError.forbidden(messages.notOwner)
+    if (!moment) throw HttpStatusError.notFound(messages.notFound)
+    if (moment.Post.madeBy !== userId)
+        throw HttpStatusError.forbidden(messages.notOwner)
+    if (!isEditable(moment.createdAt)) {
+        throw HttpStatusError.forbidden('You can\'t delete this moment after 24 hours of its creation.')
+    }
+  
+    await moment.destroy()
 
-    await sequelize.transaction(async transaction => {
-        await moment.destroy({transaction})
-    })
-
-    res.json({deleted: true})
+    res.json({ deleted: true })
 })
