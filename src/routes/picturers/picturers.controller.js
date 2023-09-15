56858d0d4c09c987f5459334c7d2c02da7bee4c0
chengkev <chengkev2000@gmail.com>
2023-09-14 22:03:06 -0400

    fix: refactor remove fix header

--- src/routes/picturers/picturers.controller.js
+++ src/routes/picturers/picturers.controller.js
@@ -5,9 +5,8 @@ const {Readable} = require('stream')
 
 module.exports.get_picturers_picture_display_key = controllerWrapper(async (req, res) => {
     const { key } = req.params
-    const {content, mime, ext} = await s3Provider.download(key, false)
+    const {content, mime} = await s3Provider.download(key, false)
     const stream = Readable.from(content)
     res.setHeader('Content-Type', mime)
-    res.setHeader('Content-Disposition', `attachment; filename="avatar.${ext}"`)
     stream.pipe(res)
 })
