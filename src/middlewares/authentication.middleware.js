d17607a2397aac0cf4c51fd4a8943907ba3a762a
chengkev <chengkev2000@gmail.com>
2023-10-01 09:56:39 -0400

    fix: rollback on verify jwt change

--- src/middlewares/authentication.middleware.js
+++ src/middlewares/authentication.middleware.js
@@ -8,9 +8,7 @@ const authentication = controllerWrapper(async (req, res, next) => {
     const bearerToken = req.headers['authorization']
     if(!bearerToken) throw HttpStatusError.unauthorize({code: 'auth', message: 'A token is required for authentication'})
     const token = bearerToken.split(' ').pop()
-    const decoded = jwt.verify(token, config.JWT_TOKEN, (err) => {
-        throw HttpStatusError.unauthorize({code: 'auth', message: 'Expired token'})
-    })
+    const decoded = jwt.verify(token, config.JWT_TOKEN)
     const user = await Users.findByPk(decoded.id)
     if(!user) throw HttpStatusError.unauthorize({code: 'auth', message: 'invalid session'})
     req.user = decoded
