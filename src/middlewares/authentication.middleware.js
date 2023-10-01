908671bdbc23183c9a5815aff86e970a08ccbe5d
chengkev <chengkev2000@gmail.com>
2023-10-01 09:43:01 -0400

    fix: expired jwt thrown status error

--- src/middlewares/authentication.middleware.js
+++ src/middlewares/authentication.middleware.js
@@ -6,11 +6,13 @@ const {Users} = require('../database/models')
 
 const authentication = controllerWrapper(async (req, res, next) => {
     const bearerToken = req.headers['authorization']
-    if(!bearerToken) throw HttpStatusError.unauthorize({message: 'A token is required for authentication'})
+    if(!bearerToken) throw HttpStatusError.unauthorize({code: 'auth', message: 'A token is required for authentication'})
     const token = bearerToken.split(' ').pop()
-    const decoded = jwt.verify(token, config.JWT_TOKEN)
+    const decoded = jwt.verify(token, config.JWT_TOKEN, (err) => {
+        throw HttpStatusError.unauthorize({code: 'auth', message: 'Expired token'})
+    })
     const user = await Users.findByPk(decoded.id)
-    if(!user) throw HttpStatusError.unauthorize({message: 'invalid session'})
+    if(!user) throw HttpStatusError.unauthorize({code: 'auth', message: 'invalid session'})
     req.user = decoded
     next()
 })
